769d639d1d87b76d37d3ec93c5db6ba185968b11
Serhii Kostiukevych <serhii.kostiukevych@lembergsolutions.com>
2024-11-05 15:39:46 +0200

    Dynamic landing page

---
+++ src/component/cards.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "./link.jsx";
+
+/**
+ * Nav renders a navigation resource.
+ *
+ * @param resource
+ */
+const Cards = ({ cards }) => {
+  console.log(cards);
+  return  <div className="cards">
+    {cards.data.map(({ title, teaserIcon, teaserText, links }, i) => (
+      <a key={i} href={links.get('canonical').href}>
+        <div key={i} className="card">
+          <div className={'card-icon ' + teaserIcon}></div>
+          <div className="card-content">
+            <div className="card-title">{title}</div>
+            <div className="card-description">{teaserText}</div>
+          </div>
+        </div>
+      </a>
+    ))}
+  </div>
+};
+
+export default Cards;
--- src/page/landing-page.jsx
+++ src/page/landing-page.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import ProcessedHTML from "../component/processed-html.jsx";
-import Nav from "../component/nav.jsx";
-
-/**
- * Section renders each item of the "landing_page" resource's "sections" field.
- */
-const Section = ({ id, heading, content }) => (
-  <section key={id}>
-    <h3>{heading}</h3>
-    <ProcessedHTML html={content} />
-  </section>
-);
+import ProcessedHTML from '../component/processed-html.jsx';
+import Nav from '../component/nav.jsx';
+import Cards from "../component/cards.jsx";
 
 /**
  * LandingPage renders "landing_page" resources.
@@ -20,17 +11,38 @@ const Section = ({ id, heading, content }) => (
  */
 const LandingPage = ({ fields }) => {
   // Extract the required fields from the resource fields.
-  const { mainMenu, hero, sections } = fields;
+  const { title, hero, mainMenu, cards } = fields;
+  console.log(fields);
   return (
     <div id="landing-page">
-      <header>
-        {/* The main menu. */}
-        <Nav resource={mainMenu.data} />
-        {/* The hero content is sent from the server, pre-processed and filtered against XSS vulnerabilities. */}
-        <ProcessedHTML html={hero} />
+      <header className="header">
+        <div className="header-content container">
+          <Nav menu={ mainMenu.data } />
+          <div className="actions">
+            <a href="#" className="btn-primary">Edit Page</a>
+          </div>
+        </div>
       </header>
-      {/* Render all sections. */}
-      <main>{sections.data.map(Section)}</main>
+      <div className="main-content">
+        <div className="banner container">
+          <h1 className="title">{ title }</h1>
+          <div className="description">
+            <ProcessedHTML html={hero} />
+          </div>
+        </div>
+        <div className="background-image-section">
+          <div className="container">
+            <div className="text-wrapper">
+              <h2 className="content-title">Dive Deeper</h2>
+              <div className="content-description">This introductory application
+                explains and demonstrates a few of Applura's features and
+                quickly describes how to use them.
+              </div>
+            </div>
+            <Cards cards={cards} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
